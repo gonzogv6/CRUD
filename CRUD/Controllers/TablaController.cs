674a9882c29d378f5cb0cc29e04979b14afef8a1
Gonzalo <Gonzalo@DESKTOP-61VFOVU>
2020-06-25 21:36:12 -0500

    Hasta 26:00

---
+++ CRUD/Controllers/TablaController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using CRUD.Models;
+using CRUD.Models.ViewModels;
+
+namespace CRUD.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<TablaViewModel> lst;
+            using (CRUDEntities db = new CRUDEntities())
+            {
+                 lst = (from d in db.tabla
+                           select new TablaViewModel
+                           {
+                               Id = d.id,
+                               Nombre = d.nombre,                               
+                               Correo = d.correo
+                           }).ToList();
+            }
+            return View(lst);
+        }
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel1 model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (CRUDEntities db = new CRUDEntities())
+                    {
+                        var oTabla = new tabla();
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+                }
+                return Redirect("Tabla/");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ CRUD/Models/ViewModels/TablaViewModel1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models.ViewModels
+{
+    public class TablaViewModel1
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name ="Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        [Display(Name = "Correo")]
+        public string Correo { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime Fecha_Nacimiento { get; set; }
+    }
+}
