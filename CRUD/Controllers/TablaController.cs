4b5e82c35660d9fbd25b0539126578318bf2fc30
Gonzalo <Gonzalo@DESKTOP-61VFOVU>
2020-06-28 11:35:40 -0500

    No inserta informacion

--- CRUD/Controllers/TablaController.cs
+++ CRUD/Controllers/TablaController.cs
@@ -48,13 +48,68 @@ namespace CRUD.Controllers
                         db.tabla.Add(oTabla);
                         db.SaveChanges();
                     }
+                    return Redirect("~/tabla/");
                 }
-                return Redirect("Tabla/");
+                
+                return View(model);
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        public ActionResult Editar(int Id)
+        {
+            TablaViewModel1 model = new TablaViewModel1();
+            using (CRUDEntities db = new CRUDEntities())
+            {
+                var oTabla = db.tabla.Find(Id);
+                model.Nombre = oTabla.nombre;
+                model.Correo = oTabla.correo;
+                model.Fecha_Nacimiento = oTabla.fecha_nacimiento;
+                model.Id = oTabla.id;
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel1 model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (CRUDEntities db = new CRUDEntities())
+                    {
+                        var oTabla = db.tabla.Find(model.Id);
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    return Redirect("~/tabla/");
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpGet]
+        public ActionResult Eliminar(int Id)
+        {
+            using (CRUDEntities db = new CRUDEntities())
+            {
+                
+                var oTabla = db.tabla.Find(Id);
+                db.tabla.Remove(oTabla);
+                db.SaveChanges();
+            }
+            return Redirect("~/tabla/");
         }
     }
 }
--- CRUD/Models/ViewModels/TablaViewModel1.cs
+++ CRUD/Models/ViewModels/TablaViewModel1.cs
@@ -11,17 +11,18 @@ namespace CRUD.Models.ViewModels
         
         public int Id { get; set; }
         [Required]
-        [StringLength(100)]
+        [StringLength(50)]
         [Display(Name ="Nombre")]
         public string Nombre { get; set; }
         [Required]
-        [StringLength(100)]
+        [StringLength(50)]
         [EmailAddress]
         [Display(Name = "Correo")]
         public string Correo { get; set; }
         [Required]
         [DataType(DataType.Date)]
         [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Fecha_Nacimiento { get; set; }
     }
 }
